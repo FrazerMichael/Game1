20e076e3c83adb9c742a7e135cfa5f81d33c584c
Michael Frazer <mjfrazer1@gmail.com>
2021-09-28 16:24:19 -0400

    added auto movement, need to slow it down!

--- game1.py
+++ game1.py
@@ -4,12 +4,16 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("game1")
 
-FPS = 60
+FPS = 10
 
-a = 30
-b = 30
+a = 450
+b = 240
 c = 30
 d = 30
+
+delta_a = 0
+
+delta_b = 0
 
 def draw_window():
         WIN.fill((220, 255, 255))
@@ -25,32 +29,42 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-            draw_window()
-            draw_rectangle()
+        global a, b, c, d, delta_a, delta_b
 
-            if event.type == pygame.KEYDOWN:
+        draw_window()
+        draw_rectangle()
 
-                if event.key == pygame.K_DOWN:
-                    global b, d
-                    b += d
-                    draw_rectangle()
-                    pygame.display.update
+        a += delta_a
+        b += delta_b
 
-                elif event.key == pygame.K_UP:
-                    b -= d
-                    draw_rectangle()
-                    pygame.display.update
+        pygame.display.update
 
-                elif event.key == pygame.K_LEFT:
-                    global a, c
-                    a -= c
-                    draw_rectangle()
-                    pygame.display.update
+        if event.type == pygame.KEYDOWN:
 
-                elif event.key == pygame.K_RIGHT:
-                    a += c
-                    draw_rectangle()
-                    pygame.display.update
+            if event.key == pygame.K_DOWN:
+                delta_a = 0
+                delta_b = c
+                draw_rectangle()
+                pygame.display.update()
+
+            elif event.key == pygame.K_UP:
+                delta_a = 0
+                delta_b = - c
+                draw_rectangle()
+                pygame.display.update
+
+            elif event.key == pygame.K_LEFT:
+                delta_a = - d
+                delta_b = 0
+                draw_rectangle()
+                pygame.display.update
+
+            elif event.key == pygame.K_RIGHT:
+                delta_a = d
+                delta_b = 0
+                draw_rectangle()
+                pygame.display.update
+
 
 
     pygame.quit()
