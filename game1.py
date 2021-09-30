120e00100c10253184269e859098c350ff5c69a1
Michael Frazer <mjfrazer1@gmail.com>
2021-09-29 23:44:40 -0400

    added auto movement, need to slow it down!

--- game1.py
+++ game1.py
@@ -1,13 +1,14 @@
 import pygame
+import random
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 900, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("game1")
 
-FPS = 10
+FPS = 7
 
 a = 450
-b = 240
+b = 300
 c = 30
 d = 30
 
@@ -15,10 +16,30 @@ delta_a = 0
 
 delta_b = 0
 
+
+
 def draw_window():
         WIN.fill((220, 255, 255))
         pygame.display.update()
 
+def draw_rectangle():
+    pygame.draw.rect(WIN, (250, 0, 0), pygame.Rect(a, b, c, d))
+    pygame.display.update()
+
+def random_x():
+    ranWidth = random.choice(range(0, WIDTH, 30))
+    return ranWidth
+
+def random_y():
+    ranHeight = random.choice(range(0, HEIGHT, 30))
+    return ranHeight
+
+def draw_food():
+    global c, d, x, y
+    pygame.draw.rect(WIN, (250, 0, 0), pygame.Rect(x, y, c, d))
+
+x = random_y()
+y = random_x()
 
 def main():
     clock = pygame.time.Clock()
@@ -33,11 +54,12 @@ def main():
 
         draw_window()
         draw_rectangle()
+        draw_food()
+
+        pygame.display.update()
 
         a += delta_a
         b += delta_b
-
-        pygame.display.update
 
         if event.type == pygame.KEYDOWN:
 
@@ -65,14 +87,7 @@ def main():
                 draw_rectangle()
                 pygame.display.update
 
-
-
     pygame.quit()
-
-def draw_rectangle():
-    pygame.draw.rect(WIN, (250, 0, 0), pygame.Rect(a, b, c, d))
-    pygame.display.flip()
-    pygame.display.update()
 
 if __name__ == "__main__":
     main()
